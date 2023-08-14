72fb8d518a90aa6b3690b935ccee2b47f9e6dabc
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-08-14 11:47:57 +0200

    added languages bar tracker
    fixed paddings and border

--- Website-Resume-Project/Website-Resume-Project/index.js
+++ Website-Resume-Project/Website-Resume-Project/index.js
@@ -35,3 +35,34 @@ window.addEventListener('scroll', () => {
     boxArrow.style.visibility = 'visible';
   }
 });
+
+
+
+// LANGUAGES BAR TRACKER
+
+const languages = [
+  { name: 'HTML', percentage: 68.2 },
+  { name: 'CSS', percentage: 27.3 },
+  { name: 'JavaScript', percentage: 4.5 },
+  { name: 'Python', percentage: 10 },
+  { name: 'BootStrap', percentage: 5 },
+  { name: 'SQL', percentage: 8 },
+  { name: 'Node.js', percentage: 6 },
+  // Add more languages as needed
+];
+
+// Update language progress bars
+const languagesList = document.querySelector('.languages-list');
+
+languages.forEach(language => {
+  const languageItem = document.createElement('li');
+  languageItem.className = 'language';
+  languageItem.innerHTML = `
+      <span class="language-name">${language.name}</span>
+      <div class="language-bar">
+          <div class="language-progress" style="width: ${language.percentage}%;"></div>
+      </div>
+      <span class="language-percentage">${language.percentage}%</span>
+  `;
+  languagesList.appendChild(languageItem);
+});
