b14ff8210c2a27638cfc441bbcb13692551c5a0d
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-08-17 16:23:06 +0200

    removed comments

--- Website-Resume-Project/Website-Resume-Project/index.js
+++ Website-Resume-Project/Website-Resume-Project/index.js
@@ -70,10 +70,8 @@ languages.forEach(language => {
 
 //BACK TO TOP BUTTON
 
-// Get the button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
